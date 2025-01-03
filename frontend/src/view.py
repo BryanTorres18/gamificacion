d2832d419e03ab707b6593891634a6c2f7036e42
BryanTorres18 <90003001+BryanTorres18@users.noreply.github.com>
2025-01-03 15:29:59 -0500

    Terminado el modulo de Formulario de Juego

--- frontend/src/view.py
+++ frontend/src/view.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html')
 
+def create_game(request, game_type):
+    return render(request, 'create_game.html', {'game_type': game_type})
+
 
